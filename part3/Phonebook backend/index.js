0c41775e2ad3c19f964daae2ceb87065523ae148
W10Go <diegoa.carvajal@udea.edu.co>
2023-11-30 23:35:56 -0500

    Commit

--- part3/Phonebook backend/index.js
+++ part3/Phonebook backend/index.js
@@ -1,7 +1,9 @@
+require('dotenv').config()
 const cors = require('cors')
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
+const Person = require('./models/person')
 
 app.use(cors())
 // Step1    Returns a hardcoded list from http://localhost:3001/api/persons ✔
@@ -13,7 +15,8 @@ app.use(cors())
 // Step6    Handle new entrys:
 //            °The name or number is missing ✔
 //            °The name already existis in the phonebook✔
-// 
+// Phonebook database
+// Step1
 
 app.use(express.json())
 
@@ -63,22 +66,23 @@ app.get('/info', (request, response) => {
 })
 
 app.get('/api/persons', (request, response) => {
-  response.json(persons)
+  //
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
 })
-app.get('/api/persons/:id', (request, response) =>{
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
 
-    if(person){
-        response.json(person)
-    } else {
-        response.status(404).end()
-    }
+
+app.get('/api/persons/:id', (request, response) =>{
+
+    Person.findById(request.params.id).then(person => {
+      response.json(person)
+    })
 })
 
 app.delete('/api/persons/:id', (request, response) => {
     const id = Number(request.params.id)
-    persons = persons.filter(note => note.id !== id)
+    persons = persons.filter(person => person.id !== id)
   
     response.status(204).end()
   })
@@ -88,35 +92,23 @@ const generateId = () => {
 }
 
 app.post('/api/persons', morgan(':method :url :status :res[content-length] - :response-time ms :data'), (request, response) =>{
-    const body = request.body
-    if(!body.name){
-      return response.status(400).json({
-        error: 'Name missing'
-      })
-    }
-    if(!body.number){
-      return response.status(400).json({
-        error: 'Number missing'
-      })
-    }
-    const repeated = persons.find(person => person.name === body.name)
-    if(repeated){
-      return response.status(400).json({
-        error: 'name must be unique'
-      })
-    }
-    const person = {
-      name: body.name,
-      number: body.number,
-      id: generateId()
-    }
+  const body = request.body
+  if (body.name === undefined) {
+    return response.status(400).json({error: 'content missing'})
+  }
 
-    persons = persons.concat(person)
+  const person = new Person({
+    name: body.name,
+    number: body.number
+  })
 
-    response.json(person)
+  person.save()
+    .then(savedPerson =>{
+      response.json(savedPerson)
+    })
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
