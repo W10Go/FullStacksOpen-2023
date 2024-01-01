0b6147bddcea6eecfca29e123ba9248c4f8505f4
W10Go <diegoa.carvajal@udea.edu.co>
2023-12-31 22:17:57 -0500

    Eslint Added

--- part3/Phonebook backend/index.js
+++ part3/Phonebook backend/index.js
@@ -68,26 +68,26 @@ app.use(requestLogger)
 
 
 let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
+  { 
+    'id': 1,
+    'name': 'Arto Hellas', 
+    'number': '040-123456'
+  },
+  { 
+    'id': 2,
+    'name': 'Ada Lovelace', 
+    'number': '39-44-5323523'
+  },
+  { 
+    'id': 3,
+    'name': 'Dan Abramov', 
+    'number': '12-43-234345'
+  },
+  { 
+    'id': 4,
+    'name': 'Mary Poppendieck', 
+    'number': '39-23-6423122'
+  }
 ]
 
 app.get('/', (request, response) => {
@@ -96,8 +96,8 @@ app.get('/', (request, response) => {
 
 app.get('/info', (request, response) => {
     
-    const date = new Date();
-    response.send(`
+  const date = new Date()
+  response.send(`
     <p>Phonebook has info for ${persons.length} people <br/>${date}</p>
     `)
 })
@@ -107,22 +107,22 @@ app.get('/api/persons', (request, response) => {
   Person.find({})
     .then(persons => {
       response.json(persons)
-  })
+    })
 })
 
-app.get('/api/persons/:id', (request, response, next) =>{
-    Person.findById(request.params.id)
-      .then(person => {
-        if (person) {
-          response.json(person)
-        } else {
-          response.status(404).end()
-        }
-      })
-      .catch(error => next(error))
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
-app.post('/api/persons', morgan(':method :url :status :res[content-length] - :response-time ms :data'), (request, response, next) =>{
+app.post('/api/persons', morgan(':method :url :status :res[content-length] - :response-time ms :data'), (request, response, next) => {
   const body = request.body
   if (body.name === undefined) {
     return response.status(400).json({error: 'name missing'})
@@ -134,21 +134,21 @@ app.post('/api/persons', morgan(':method :url :status :res[content-length] - :re
   })
 
   person.save()
-    .then(savedPerson =>{
+    .then(savedPerson => {
       response.json(savedPerson)
     })
     .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
-    Person.findByIdAndDelete(request.params.id)
-      .then(result => {
-        response.status(204).end()
-      })
-      .catch(error => next(error))
-  })
+  Person.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
 
-app.put('/api/persons/:id', (request, response, next) =>{
+app.put('/api/persons/:id', (request, response, next) => {
   const {bname, bnumber} = request.body
 
   Person.findByIdAndUpdate(request.params.id, {bname, bnumber}, { new: true, runValidators: true, bname: 'query' })
